7fdd3fe094627a0148cc1dd6ba4a6a2b3498c3f0
Ardin Fardiansyah <ardfarde@gmail.com>
2023-01-11 00:21:24 +0700

    add tmux rnix-lsp

--- home.nix
+++ home.nix
@@ -2,6 +2,10 @@
   home.username = "ardfard";
   home.homeDirectory = "/home/ardfard";
   home.stateVersion = "22.11";
+  home.packages = [
+    pkgs.tmux
+    pkgs.rnix-lsp
+  ];
   programs.home-manager.enable = true;
   programs.neovim.enable = true;
 }
