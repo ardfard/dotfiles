c99b7750285c832cc1c9a4940432f4968d2408df
Ardin Fardiansyah <ardfarde@gmail.com>
2023-01-11 00:43:09 +0700

    add zsh git

--- home.nix
+++ home.nix
@@ -8,4 +8,15 @@
   ];
   programs.home-manager.enable = true;
   programs.neovim.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "ardfard";
+    userEmail = "ardfarde@gmail.com";
+  };
+  programs.zsh = {
+    oh-my-zsh = {
+      plugins = [ "git" "zsh-autosuggestions" "zsh-syntax-highlighting" ];
+    };
+    enable = true;
+  };
 }
