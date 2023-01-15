0ea226b2a72d668752da4e540b01b1f283bb1c5b
Ardin Fardiansyah <ardfarde@gmail.com>
2023-01-15 15:42:27 +0700

    fix home manager

--- hosts/default.nix
+++ hosts/default.nix
@@ -15,7 +15,7 @@
       home-manager.nixosModules.home-manager
       {
         home-manager.useGlobalPkgs = true;
-        home-manager-useUserPackages = true;
+        home-manager.useUserPackages = true;
         home-manager.extraSpecialArgs = {
           inherit user;
           host = {
