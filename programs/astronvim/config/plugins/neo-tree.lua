7065c949e85b188d8f8edd036b12ed066efabd35
Ardin Fardiansyah <ardfarde@gmail.com>
2023-01-25 12:15:00 +0700

    fix neo-tree options

--- programs/astronvim/config/plugins/neo-tree.lua
+++ programs/astronvim/config/plugins/neo-tree.lua
@@ -1,7 +1,9 @@
 return {
   filesystem = {
-    visible = true,
-    hide_dotfiles = false,
-    hide_gitignored = false,
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = false,
+      hide_gitignored = false,
+    }
   }
 }
